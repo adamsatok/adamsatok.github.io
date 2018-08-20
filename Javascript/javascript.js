e845d2f4878a7be15517e3ca2c0efa2f70810e37
adamsatok <adamsatok@gmail.com>
2018-08-19 22:03:22 -0400

    updated for Winter 2019 job search

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -9,11 +9,15 @@ $(document).ready(function () {
 	$(".subsection").hover(function(){
 		var highlightable = true;
 		switch (this.id) {
+			case "Influitive": highlight = Influitive;
+			break;
 			case "Mitre": highlight = Mitre;
 			break;
 			case "Metroland": highlight = Metroland;
 			break;
 			case "BrandSpark": highlight = BrandSpark;
+			break;
+			case "Cannon Blast": highlight = CannonBlast;
 			break;
 			case "Ball Dash": highlight = BallDash;
 			break;
@@ -37,11 +41,12 @@ $(document).ready(function () {
 		$(highlight).css("background-color", "#8c8c8c");
 	});
 });
-
+Influitive = "#Ruby, #JS, #HTML, #CSS, #SQL, #RubyonRails, #Linux, #Git, #Vim, #Postgres, #Docker";
 Mitre = "#Ruby, #HTML, #CSS, #JS, #SQL, #RubyonRails, #PHP, #Linux, #Git, #SequelPro, #Vim";
 Metroland = "#PHP, #HTML, #CSS, #JS, #SQL, #Python, #Yii, #Linux, #Git, #SequelPro, #Selenium, #Vim";
 BrandSpark = "#PHP, #HTML, #CSS, #JS, #Java, #SQL, #Git";
-BallDash = "#Swift, #Git"
+CannonBlast = "#C3, #Unity, #Git";
+BallDash = "#Swift, #Git, #SpriteKit";
 HatetheWait = "#PHP, #HTML, #CSS, #JS, #SQL, #Git, #Linux, #Vim";
 CubicleCoder = "#HTML, #CSS, #JS, #Git, #Linux, #Vim";
 Education = "#C, #C2, #Linux, #Vim, #Git";
