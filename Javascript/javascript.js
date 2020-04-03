8dbe92b8bc827824a1084b114f943adf1cbe2167
Adam Satok <adamsatok@gmail.com>
2020-04-03 00:48:02 -0400

    updating for new grad job search

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -9,25 +9,21 @@ $(document).ready(function () {
 	$(".subsection").hover(function(){
 		var highlightable = true;
 		switch (this.id) {
+			case "Postmates": highlight = Postmates;
+			break;
+			case "Amazon": highlight = Amazon;
+			break;
 			case "Zazzle": highlight = Zazzle;
 			break;
 			case "Influitive": highlight = Influitive;
 			break;
-			case "Mitre": highlight = Mitre;
-			break;
-			case "Metroland": highlight = Metroland;
-			break;
-			case "BrandSpark": highlight = BrandSpark;
+			case "GameTheory": highlight = GameTheory;
 			break;
 			case "Cannon Blast": highlight = CannonBlast;
 			break;
 			case "Ball Dash": highlight = BallDash;
 			break;
-			case "HatetheWait": highlight = HatetheWait;
-			break;
-			case "CubicleCoder": highlight = CubicleCoder;
-			break;
-			case "Education": highlight = Education;
+			case "Education": highlightable = false;
 			break;
 			case "Leadership": highlightable = false;
 			break;
@@ -44,14 +40,12 @@ $(document).ready(function () {
 	});
 });
 
-Zazzle = "#C3, #SQL, #HTML, #CSS, #JS, #Net, #Git, #AWS, #Jenkins, #Linux";
+Postmates = "#Python, #Golang, #SQL, #Git, #Linux, #Git, #AWS, #Kube, #gRPC"
+Amazon = "#Python, #JS, #HTML, #CSS, #SQL, #Git, #Linux, #AWS, #React"
+Zazzle = "#C3, #SQL, #HTML, #CSS, #JS, #Net, #Git, #AWS, #Jenkins, #Linux, #gRPC";
 Influitive = "#Ruby, #JS, #HTML, #CSS, #SQL, #RubyonRails, #Linux, #Git, #Vim, #Postgres, #Docker";
-Mitre = "#Ruby, #HTML, #CSS, #JS, #SQL, #RubyonRails, #PHP, #Linux, #Git, #SequelPro, #Vim";
-Metroland = "#PHP, #HTML, #CSS, #JS, #SQL, #Python, #Yii, #Linux, #Git, #SequelPro, #Selenium, #Vim";
-BrandSpark = "#PHP, #HTML, #CSS, #JS, #Java, #SQL, #Git";
+GameTheory = "#Python, #Git, #Linux"
 CannonBlast = "#C3, #Unity, #Git";
 BallDash = "#Swift, #Git, #SpriteKit";
-HatetheWait = "#PHP, #HTML, #CSS, #JS, #SQL, #Git, #Linux, #Vim";
-CubicleCoder = "#HTML, #CSS, #JS, #Git, #Linux, #Vim";
 Education = "#C, #C2, #Linux, #Vim, #Git";
 Leadership = "";
